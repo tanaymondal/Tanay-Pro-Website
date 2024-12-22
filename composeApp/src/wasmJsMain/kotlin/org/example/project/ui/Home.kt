35579174293614d71f7d98b4c31cfbcbb19a1946
Tanay Mondal <tanaymondal6@gmail.com>
2024-12-22 10:41:24 +0530

    Image issue fixed

--- composeApp/src/wasmJsMain/kotlin/org/example/project/ui/Home.kt
+++ composeApp/src/wasmJsMain/kotlin/org/example/project/ui/Home.kt
@@ -9,16 +9,17 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,9 +27,19 @@ import org.jetbrains.compose.resources.painterResource
 import tanay_website.composeapp.generated.resources.Res
 import tanay_website.composeapp.generated.resources.image
 
+
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun showHome() {
     SelectionContainer {
+
+        val width = LocalWindowInfo.current.containerSize.component1()
+        val height = LocalWindowInfo.current.containerSize.component2()
+
+        val ratio = width.toFloat() / height.toFloat()
+        val imageSize = ratio * 400
+        val textSize = ratio * 30
+
         Row(
             modifier = Modifier.fillMaxSize(),
             horizontalArrangement = Arrangement.Center,
@@ -40,9 +51,10 @@ fun showHome() {
             Text(
                 text = "Tanay\nMondal",
                 textAlign = TextAlign.End,
-                fontSize = 40.sp,
-                lineHeight = 40.sp,
-                color = Color.White
+                fontSize = textSize.sp,
+                lineHeight = textSize.sp,
+                color = Color.White,
+                maxLines = 2
             )
 
             Spacer(modifier = Modifier.size(10.dp))
@@ -52,12 +64,7 @@ fun showHome() {
                 contentDescription = "",
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .sizeIn(
-                        minHeight = 200.dp,
-                        minWidth = 200.dp,
-                        maxWidth = 500.dp,
-                        maxHeight = 500.dp
-                    )
+                    .size(imageSize.dp)
                     .border(
                         BorderStroke(borderWidth, Color.Yellow),
                         CircleShape
@@ -71,9 +78,10 @@ fun showHome() {
             Text(
                 text = "Mobile\nDeveloper",
                 textAlign = TextAlign.Start,
-                fontSize = 40.sp,
-                lineHeight = 40.sp,
-                color = Color.White
+                fontSize = textSize.sp,
+                lineHeight = textSize.sp,
+                color = Color.White,
+                maxLines = 2
             )
         }
     }
